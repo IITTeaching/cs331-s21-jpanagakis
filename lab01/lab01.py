778873aa3e7ffdb6739d2f4280c5ab57fd577096
jpanagakis2309 <77741781+jpanagakis2309@users.noreply.github.com>
2021-01-30 18:00:43 -0600

    Update lab01.py

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -22,7 +22,15 @@ def captured_output():
 
 # implement this function
 def is_perfect(n):
-    pass
+    sum = 0
+    for i in range(1,n):
+        if n % i == 0:
+            sum += i
+            
+    if sum == n:
+        return True
+    else:
+        return False
 
 # (3 points)
 def test1():
@@ -40,7 +48,11 @@ def test1():
 
 # implement this function
 def multiples_of_3_and_5(n):
-    pass
+    sum = 0
+    for i in range(1,n):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    return sum
 
 # (3 points)
 def test2():
@@ -53,7 +65,13 @@ def test2():
 # EXERCISE 3
 #################################################################################
 def integer_right_triangles(p):
-    pass
+    triNum = 0
+    for a in range(3, p):
+        for b in range(4, p):
+            c = (a**2 + b**2)**.5
+            if a + b + c == p and a < b < c:
+                triNum += 1
+    return triNum
 
 def test3():
     tc = unittest.TestCase()
@@ -67,7 +85,26 @@ def test3():
 
 # implement this function
 def gen_pattern(chars):
-    pass
+    length = len(chars)
+    bigLine = 2*(length + length - 1) - 1
+    
+    print(chars[-1].center(bigLine,'.'))
+    
+    for i in range(2,length + 1):
+        line = ''
+        for j in range(1,i + 1):
+                line += chars[-j]
+        for k in range(1,i):
+                line += chars[k-i]
+        print('.'.join(line).center(bigLine,'.'))
+        
+    for i in range(length - 1, 0, -1):
+        line = ''
+        for j in range(1,i + 1):
+                line += chars[-j]
+        for k in range(1,i):
+                line += chars[k-i]
+        print('.'.join(line).center(bigLine,'.'))
 
 def test4():
     tc = unittest.TestCase()
